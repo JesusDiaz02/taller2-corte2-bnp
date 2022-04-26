4828c63d0c343769bcdd379048c2f4b41e870a76
Jesus Diaz <jesusdiazg9@gmail.com>
2022-04-25 20:34:36 -0500

    tercer commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import {firebase} from '../firebase'
+import {nanoid} from 'nanoid';
+
+const Formulario =()=>{
+    const[nombre, setNombre] =React.useState('')
+    const[apellido, setApellido]=React.useState('')
+    const[musica, setMusica]=React.useState('')
+    const[equipoFutbol, setEquipoFutbol]=React.useState('')
+    const[pais, setPais]=React.useState('')
+    const[lista, setLista]=React.useState([])
+    const[modoEdicion, setModoEdicion]=React.useState(false)
+    const[id, setId]=React.useState('')
+    const[error, setError]=React.useState(null)
